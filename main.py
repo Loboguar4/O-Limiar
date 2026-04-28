99eec915e98a73c5d22ad5d9697db03adc133d62
Bandeirinha <49231373+Loboguar4@users.noreply.github.com>
2026-04-28 16:47:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -484,7 +484,7 @@ PESOS_ITENS = {
     # ── LATEGAME — Equipamentos do Caçador ───────────────────────────
     'Botas do Caçador de Monstros': 1.5,
     'Elmo do Caçador de Monstros':  2.5,
-    # ── NOVOS — Itens Mágicos ─────────────────────────────────────────
+    # ── NOVOS ─────────────────────────────────────────
     'Botas Encantadas':             0.8,
     'Cajado do Fogo Descendente':   1.6,
     'Códice dos Segredos Elementais': 1.8,
